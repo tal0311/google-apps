0610564b6ec3a72b71bd10cfcb9c3db7ef65dd72
tal0311 <tal.amit0311@gmail.com>
2023-07-17 17:57:14 +0300

    loader wont show in main page

--- cmps/AppLoader.js
+++ cmps/AppLoader.js
@@ -2,14 +2,13 @@ export default {
   name: 'NotesLoader',
   props: ['isLoading'],
   template: `
-          <div v-if="isLoading" class="app-loader"></div>
-          <!-- <div v-if="!isLoading">
-            <i class="material-symbols-outlined">refresh</i>
-          </div> -->
-          <div v-else>
-            <i class="material-symbols-outlined">cloud_done</i>
+          <div  class="app-loader">
+            <div v-if="isLoading" class="loader"></div>
+            <div v-else>
+              <i class="material-symbols-outlined">cloud_done</i>
+            </div>
           </div>
-        `,
+                `,
   components: {},
   created() { },
   data() {
