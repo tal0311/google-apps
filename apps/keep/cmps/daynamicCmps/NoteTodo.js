87c94b27e8e4c90a8224db13651c1f8c5aa8fc78
tal0311 <tal.amit0311@gmail.com>
2023-07-22 21:21:18 +0300

    todo updateinfo

--- apps/keep/cmps/daynamicCmps/NoteTodo.js
+++ apps/keep/cmps/daynamicCmps/NoteTodo.js
@@ -1,21 +1,46 @@
 export default {
   name: 'NoteTodo',
   props: ['info'],
+  emits: ['updateInfo'],
   template: `
          <article class="note-dynamic note-todo ">
+         
          <ul>
-           <li :class="['todo-item', todo.donAt? 'done':'']" v-for="todo in info.todos" :key="todo.id">
-            <p>{{todo.txt}}</p>
-            <input type="checkbox" :checked="todo.isChecked" name=""  id="" />         
+           <li :class="['todo-item grid', todo.donAt? 'done':'']" v-for="todo,idx in info.todos" :key="todo.id">
+             <input type="checkbox" @change="updateDone(idx)" :checked="isChecked(idx)" name=""  id="" />         
+            <p :contenteditable="$attrs.isDetails"  @blur="updateTodoTitle(idx, $event)">{{todo.txt}}</p>
           </li>
          </ul>
          </article>
         `,
   components: {},
-  created() { },
-  data() {
-    return {}
+  created() {
+
   },
-  methods: {},
-  computed: {},
+  data() {
+    return {
+      newInfo: { ...this.info }
+    }
+  },
+  methods: {
+    updateTodoTitle(idx, { target: { innerText } }) {
+      this.newInfo.todos[idx].txt = innerText
+      this.updateNote()
+    },
+    updateDone(idx) {
+      this.newInfo.todos[idx].doneAt = this.newInfo.todos[idx].doneAt ? null : Date.now()
+      console.log('this.newInfo.todos:', this.newInfo.todos)
+      this.updateNote()
+    },
+    isChecked(idx) {
+      return this.info.todos[idx].doneAt ? true : false
+    },
+    updateNote() {
+      const infoToUpdate = JSON.parse(JSON.stringify(this.newInfo))
+      this.$emit('updateInfo', { type: 'NoteTxt', info: infoToUpdate })
+    }
+  },
+  computed: {
+
+  },
 }
