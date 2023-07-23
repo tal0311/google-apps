09c49368bda3be5a4b7ab1fe06bc2f4c000d9d19
tal0311 <tal.amit0311@gmail.com>
2023-07-23 14:14:38 +0300

    links in home page icons

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,9 +2,15 @@ import { utilService } from './../services/util.service.js'
 export default {
     template: `
         <section class="home grid">
-            <img src="./assets/img/gmail-big.png" alt="Gmail" />
-            <img src="./assets/img/keep-big.png" alt="Keep" />
-            <img src="./assets/img/Youtube-big.png" alt="YouTube" />
+            <RouterLink to="/mail?tab=inbox">
+                <img src="./assets/img/gmail-big.png" alt="Gmail" />
+           </RouterLink>
+            <RouterLink to="/note#home">
+                <img src="./assets/img/keep-big.png" alt="Keep" />
+            </RouterLink>
+            <RouterLink to="/youtube">
+                <img src="./assets/img/Youtube-big.png" alt="YouTube" />
+            </RouterLink>
         </section>
     `,
     data() {
