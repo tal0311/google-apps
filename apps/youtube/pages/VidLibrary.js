6ca21c65b718d5404763e9d254691fc030003126
tal0311 <tal.amit0311@gmail.com>
2023-08-07 11:46:34 +0300

    libreby history and liked vids

--- apps/youtube/pages/VidLibrary.js
+++ apps/youtube/pages/VidLibrary.js
@@ -6,30 +6,35 @@ export default {
     name: 'VidLibrary',
     props: [],
     template: `
-  <section class="vid-Library">
-    <div class="list-container history-list-container">
-        <div class="list-header grid">
-            <h3>history list</h3>
-            <i class="material-symbols-outlined">history</i>
-            <pre>{{user.searchHistory}}</pre>
-        </div>
-    </div>
-    <div class="list-container watch-later-container">
-          <div class="list-header grid">
-              <h3>watch later list</h3>
-              <i class="material-symbols-outlined">schedule</i>
-               <pre>{{user.watchLater}}</pre>
+  <section class="vid-Library grid">
+    <section class="list-container history-list-container">
+            <div class="list-header grid">
+              <h3>history list</h3>
+              <i class="material-symbols-outlined">history</i>
             </div>
-    </div>
-    <div v-if="user.likedVideos" class="list-container liked-videos-container grid">
+             <div class="watch-list grid">
+                <VideoPreview v-for="vid in user.searchHistory" :video="vid"/>
+             </div>
+    </section>
+    <section class="list-container watch-later-container">
+            <div class="list-header grid">
+              <h3>watch later list</h3>
+              
+            </div>
+             <div class="watch-list grid">
+                <VideoPreview v-for="vid in user.watchLater" :video="vid"/>
+             </div>
+
+    </section>
+    <section v-if="user.likedVideos" class="list-container liked-videos-container grid">
           <div class="list-header grid">
               <h3>liked videos list</h3>
               <i class="material-symbols-outlined">thumb_up</i>
         </div>
-              <div class="liked-list grid">
-                <VideoPreview v-for="vid in user.likedVideos" :video="vid"/>
-             </div>
-    </div>
+        <div class="liked-list grid">
+           <VideoPreview v-for="vid in user.likedVideos" :video="vid"/>
+        </div>
+    </section>
   </section>
         `,
     components: {
